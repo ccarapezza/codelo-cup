9ba496e25cdba37613c527119acb06f81150fc7c
ccarapezza <carapezza.christian@gmail.com>
2021-10-23 19:19:43 -0300

    kerolu endpoints

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import { createTheme } from '@mui/material/styles';
 import { ThemeProvider } from "@emotion/react";
 import { blueGrey, deepOrange } from "@mui/material/colors";
 
-//axios.defaults.baseURL = "https://codelo-cup-api.herokuapp.com/";
-axios.defaults.baseURL = "http://localhost:8080/";
+axios.defaults.baseURL = "https://codelo-cup-api.herokuapp.com/";
+//axios.defaults.baseURL = "http://localhost:8080/";
 axios.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
 
 // Add a response interceptor
